ea55bdc37dd78c3f7a54a6f0e7bd7d3a162ceada
Dhiaeddine Gharsallah <dhiaeddine.gharsallah@gmail.com>
2022-07-17 09:17:13 +0100

    Update BIT.cpp

--- Data structures/BIT.cpp
+++ Data structures/BIT.cpp
@@ -1,52 +1,51 @@
-#include <iostream>
-#include <assert.h>
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-#include <vector>
-#include <set>
-#include <map>
-#include <queue>
-#include <stack>
-#include <string>
-#include <string.h>
-#include <cmath>
-#include <memory.h>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-const int N = 2e5 + 2;
-
-int n, BIT[N], a[N];
-
-void update(int idx, int val) {
-	while (idx <= n) {
-		BIT[idx] += val;
-		idx += idx & -idx;
-	}
-}
-int get(int idx) {
-	int sum = 0;
-	while (idx > 0) {
-		sum += BIT[idx];
-		idx -= idx & -idx;
-	}
-	return sum;
-}
+class BIT {
+public:
+    BIT(vector<int> a) { 
+        this->a = {0};
+        this->bit.resize(2*a.size());
+        for (int i = 0; i < a.size(); ++i) {
+            this->a.push_back(a[i]);
+        }
+        for (int i = 1; i < this->a.size(); ++i)
+            update(i, this->a[i]);
+    }
+    void update(int idx, int val) {
+        while (idx <= bit.size()) {
+            bit[idx] += val;
+            idx += idx & -idx;
+        }
+    }
+    int get(int idx) {
+        int sm = 0;
+        while(idx > 0) {
+            sm += bit[idx];
+            idx -= idx & -idx;
+        }
+        return sm;
+    }
+private:
+    vector<int> a;
+    vector<int> bit;
+};
 
 int main() {
-	cin >> n;
-	for (int i = 1; i <= n; ++i) {
-		cin >> a[i];
-		update(i, a[i]);
+    BIT b = BIT({1,2,3,4,5});
+    vector<pair<int,int>> queries = {{1,1},{2,2},{1,2},{1,5}};
+    vector<int> out = {1,2,3,15};
+	for (int i = 0; i < queries.size(); ++i) {
+		int l = queries[i].first;
+        int r = queries[i].second;
+		int res = b.get(r) - b.get(l - 1);
+		if (res != out[i]) {
+            cout << "FAILED at testcase " << i+1 << "\n";
+            cout << "Expected " << out[i] << "\n";
+            cout << "Found " << res << "\n";
+            return 0;
+        }
 	}
-	int q, i, j;
-	cin >> q;
-	while (q--) {
-		cin >> i >> j;
-		int res = get(j) - get(i - 1);
-		cout << res << endl;
-	}
+    cout << "PASSED!\n";
 	return 0;
 }
-
